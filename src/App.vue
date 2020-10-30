2ef5f0601764c38d423970d46283605d82006318
Anzelika Häberli <anzelika@joineer.ch>
2020-10-31 00:01:00 +0100

    footer updates

--- src/App.vue
+++ src/App.vue
@@ -15,24 +15,32 @@
         </v-container>
 
         <v-footer
+        padless
           :color="$vuetify.breakpoint.xs ? 'primary darken-1' : 'transparent'"
           :dark="$vuetify.breakpoint.xs"
-          class="mt-16"
+          class="mt-14 pt-5"
           v-show="$route.name === 'home'"
         >
           <!-- <div style="height: 5px; background-color: black; width: 100%"></div> -->
           <!-- <div
             style="height: 3px; background-color: black; margin-top: 2px; width: 100%"
           ></div> -->
-          <v-row justify="center">
-            <v-col class="text-center">
+          <v-row justify="center no-gutters">
+            <v-col class="text-center ma-0">
               <h3 class="mb-1">{{ $t("home.head") }}</h3>EVATHERM Ltd. | Hintermättlistr. 3 | Gewerbepark | CH-5506 Mägenwil
               <div v-for="contact in contactInfo" :key="contact.text">
                 <v-icon small class="mr-1">{{ contact.icon }}</v-icon>
                 <span>{{ contact.text }}</span>
               </div>
+
+
+               <div class="mt-10 text-caption"  :class="$vuetify.breakpoint.xs ? 'primary' : 'grey'">
+            <a class="text-decoration-none" :class="$vuetify.breakpoint.xs ? 'blue--text text--lighten-2' : 'black--text'" target="_blank" href="https://anzudev.com">© Anzelika Häberli  -  {{ new Date().getFullYear() }}</a>
+          </div>
             </v-col>
           </v-row>
+
+         
         </v-footer>
       </v-main>
     </v-app>
